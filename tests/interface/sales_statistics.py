2dd6659866d807bab117d0584e45a93fe8129619
joelww <15183691117@163.com>
2017-12-13 20:45:36 +0800

    Add the use case configuration file

---
+++ tests/interface/sales_statistics.py
@@ -0,0 +1,11 @@
+__author__ = 'messageoom'
+
+from config import globalparam
+from public.requests_wrapper import base
+
+def getcalllist_001():
+    pass
+def getcalllist_002():
+    pass
+def getcalllist_003():
+    pass
